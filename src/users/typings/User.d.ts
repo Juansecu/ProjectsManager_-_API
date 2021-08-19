a18683a9fca853eedd8a9e5eaa6b722793cbb44e
Juansecu <juansecoder@hotmail.com>
2021-08-19 11:20:54 -0500

    Typings for User were added into UsersModule

---
+++ src/users/typings/User.d.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  userId: string;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  registeredAt: Date;
+  updatedAt: Date;
+}
