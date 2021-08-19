eeb1ba47517ca92e03de8295939da32ed27b4153
Juansecu <juansecoder@hotmail.com>
2021-08-19 16:42:00 -0500

    AddUserDto was added into UsersModule

---
+++ src/users/dtos/add-user.dto.ts
@@ -0,0 +1,9 @@
+import { IsEmail, IsString, Length, Matches, MaxLength } from 'class-validator';
+
+export class AddUserDto {
+  @IsString() @Matches(/^[a-zA-Z0-9]{15}$/) username: string;
+  @IsString() @IsEmail() @MaxLength(45) email: string;
+  @IsString() @Matches(/^[a-zA-Z]{2,20}$/) firstName: string;
+  @IsString() @Matches(/^[a-zA-Z]{3,20}$/) lastName: string;
+  @IsString() @Length(8, 16) password: string;
+}
