71469f454e5d7658269d36ff28b3a95338d643a9
Juansecu <juansecoder@hotmail.com>
2021-08-19 11:48:51 -0500

    UserHistoryEntity from UsersModule was setted

---
+++ src/users/entities/user-history.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+import { IUserHistory } from '../typings/UserHistory';
+
+@Entity('User_histories')
+export default class UserHistoryEntity implements IUserHistory {
+  @PrimaryGeneratedColumn('uuid', { name: 'User_history_id' })
+  userHistoryId: string;
+  @Column('varchar', { length: 45, name: 'Title', nullable: false })
+  title: string;
+  @Column('uuid', { name: 'User_id', nullable: false }) userId: string;
+  @Column('datetime', { default: 'CURRENT_TIMESTAMP', name: 'Created_at' })
+  createdAt: Date;
+  @Column('datetime', { name: 'Updated_at' }) updatedAt: Date;
+}
