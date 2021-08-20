bf4227514cd33094beded9429d32811da15a24c6
Juansecu <juansecoder@hotmail.com>
2021-08-19 19:29:37 -0500

    Repositories from User and UserHistory were enabled to be used by UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import UserEntity from './entities/user.entity';
+import UserHistoryEntity from './entities/user-history.entity';
 
 import { UsersController } from './users.controller';
 
 import { UsersService } from './services/users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity, UserHistoryEntity])],
   controllers: [UsersController],
   providers: [UsersService]
 })
