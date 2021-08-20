e0e4d412aea2ebe7c491b98ba7c124e21a574f5b
Juansecu <juansecoder@hotmail.com>
2021-08-19 19:33:52 -0500

    Registration for users was implemented into UsersModule

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import UserEntity from '../entities/user.entity';
+import { AddUserDto } from '../dtos/add-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly _USERS_REPOSITORY: Repository<UserEntity>
+  ) {}
+
+  addUser(user: AddUserDto) {
+    return this._USERS_REPOSITORY
+      .insert(user)
+      .then(user => {
+        return {
+          code: 201,
+          message: 'User created',
+          data: user
+        };
+      })
+      .catch((error: Error) => {
+        console.error(typeof Error, error.message);
+        return {
+          code: 500,
+          message: 'Hubo un error al crear el usuario'
+        };
+      });
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { AddUserDto } from './dtos/add-user.dto';
+
+import { UsersService } from './services/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly _USERS_SERVICE: UsersService) {}
+
+  @Post('register')
+  addUser(@Body() newUser: AddUserDto) {
+    return this._USERS_SERVICE.addUser(newUser);
+  }
+}
