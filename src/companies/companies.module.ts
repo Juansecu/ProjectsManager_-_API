3d0b3e8ceda7084f011a47b1c295849a64afd9f7
Juansecu <juansecoder@hotmail.com>
2021-08-20 18:16:49 -0500

    Repository from CompanyEntity was enabled to be used by CompaniesModule

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import CompanyEntity from './entities/company.entity';
 
 import { CompaniesController } from './companies.controller';
 
 import { CompaniesService } from './services/companies.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CompanyEntity])],
   controllers: [CompaniesController],
   providers: [CompaniesService]
 })
