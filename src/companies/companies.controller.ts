dd62a53fc966c265e4324fc82f0508fb6d366896
Juansecu <juansecoder@hotmail.com>
2021-08-20 18:19:22 -0500

    getCompanies method was implemented into CompaniesController and CompaniesService from CompaniesModule

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+
+import CompanyEntity from './entities/company.entity';
+
+import { CompaniesService } from './services/companies.service';
 
 @Controller('companies')
-export class CompaniesController {}
+export class CompaniesController {
+  constructor(private readonly _COMPANIES_SERVICE: CompaniesService) {}
+
+  @Get()
+  getCompanies(): Promise<CompanyEntity[]> {
+    return this._COMPANIES_SERVICE.getCompanies();
+  }
+}
--- src/companies/services/companies.service.ts
+++ src/companies/services/companies.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import CompanyEntity from '../entities/company.entity';
 
 @Injectable()
-export class CompaniesService {}
+export class CompaniesService {
+  constructor(
+    @InjectRepository(CompanyEntity)
+    private readonly _COMPANIES_REPOSITORY: Repository<CompanyEntity>
+  ) {}
+
+  getCompanies(): Promise<CompanyEntity[]> {
+    return this._COMPANIES_REPOSITORY.find();
+  }
+}
