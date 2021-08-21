8041b09c0779c5c05719081190421a1c52ff8b72
Juansecu <juansecoder@hotmail.com>
2021-08-21 17:30:17 -0500

    createUserHistory method was implemented into UserHistoriesController and UserHistoriesService from UserHistoriesModule

--- src/user-histories/services/user-histories.service.ts
+++ src/user-histories/services/user-histories.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import UserHistoryEntity from '../entities/user-history.entity';
+
+import { CreateUserHistoryDto } from '../dtos/create-user-history.dto';
 
 @Injectable()
-export class UserHistoriesService {}
+export class UserHistoriesService {
+  constructor(
+    @InjectRepository(UserHistoryEntity)
+    private readonly _USER_HISTORIES_REPOSITORY: Repository<UserHistoryEntity>
+  ) {}
+
+  async createUserHistory(userHistory: CreateUserHistoryDto) {
+    try {
+      await this._USER_HISTORIES_REPOSITORY.insert(userHistory);
+      return {
+        code: 201,
+        message: 'Historia de usuario ha sido creada satisfactoriamente'
+      };
+    } catch (error) {
+      console.log('\x1b[31m%s\x1b[0m', error.code, error.message);
+      return {
+        code: 500,
+        message: 'Ha habido un error creando la historia de usuario'
+      };
+    }
+  }
+}
--- src/user-histories/user-histories.controller.ts
+++ src/user-histories/user-histories.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { CreateUserHistoryDto } from './dtos/create-user-history.dto';
+
+import { UserHistoriesService } from './services/user-histories.service';
 
 @Controller('user-histories')
-export class UserHistoriesController {}
+export class UserHistoriesController {
+  constructor(
+    private readonly _USERS_HISTORIES_SERVICE: UserHistoriesService
+  ) {}
+
+  @Post('create')
+  async createUserHistory(@Body() newUserHistory: CreateUserHistoryDto) {
+    return await this._USERS_HISTORIES_SERVICE.createUserHistory(
+      newUserHistory
+    );
+  }
+}
