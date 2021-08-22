87d683d2454fbb3666936fd2358b7bc1dfbafa41
Juansecu <juansecoder@hotmail.com>
2021-08-22 06:37:37 -0500

    Credentials for the remote database were added

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,20 +11,27 @@ import { TicketsModule } from './tickets/tickets.module';
 import { UsersModule } from './users/users.module';
 import { UserHistoriesModule } from './user-histories/user-histories.module';
 
+const { CLEARDB_DATABASE_URL } = process.env;
+
+const databaseCredentials = CLEARDB_DATABASE_URL.split('://').shift();
+const databaseEP = databaseCredentials.split('@')[0];
+const databaseHN = databaseCredentials.split('@')[1];
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      database: process.env.DATABASE_NAME,
+      database:
+        databaseHN.split('/')[1].split('?')[0] || process.env.DATABASE_NAME,
       dropSchema: true,
       entities: [__dirname + '/**/**/*.entity.{ts,js}'],
-      host: process.env.DATABASE_HOST,
+      host: databaseHN.split('/')[0] || process.env.DATABASE_HOST,
       migrationsRun: true,
-      password: process.env.DATABASE_PASSWORD,
+      password: databaseEP.split(':')[1] || process.env.DATABASE_PASSWORD,
       port: parseInt(process.env.DATABASE_PORT, 10),
       synchronize: true,
       type: 'mysql',
-      username: process.env.DATABASE_USER
+      username: databaseEP.split(':')[0] || process.env.DATABASE_USER
     }),
     CompaniesModule,
     ProjectsModule,
