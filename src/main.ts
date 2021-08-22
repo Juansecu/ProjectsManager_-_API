a479851bf8eb1d57111942c2622200f5240c7faa
Juansecu <juansecoder@hotmail.com>
2021-08-22 05:40:13 -0500

    Port configuration was fixed

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT, 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
