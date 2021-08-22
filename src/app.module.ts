febc0cc780f1506bcb36223d1179b77da5452eff
Juansecu <juansecoder@hotmail.com>
2021-08-22 11:36:51 -0500

    Environment variables from ClearDB were removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,27 +11,20 @@ import { TicketsModule } from './tickets/tickets.module';
 import { UsersModule } from './users/users.module';
 import { UserHistoriesModule } from './user-histories/user-histories.module';
 
-const { CLEARDB_DATABASE_URL } = process.env;
-
-const databaseCredentials = CLEARDB_DATABASE_URL.split('://').shift();
-const databaseEP = databaseCredentials.split('@')[0];
-const databaseHN = databaseCredentials.split('@')[1];
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      database:
-        databaseHN.split('/')[1].split('?')[0] || process.env.DATABASE_NAME,
+      database: process.env.DATABASE_NAME,
       dropSchema: true,
       entities: [__dirname + '/**/**/*.entity.{ts,js}'],
-      host: databaseHN.split('/')[0] || process.env.DATABASE_HOST,
+      host: process.env.DATABASE_HOST,
       migrationsRun: true,
-      password: databaseEP.split(':')[1] || process.env.DATABASE_PASSWORD,
+      password: process.env.DATABASE_PASSWORD,
       port: parseInt(process.env.DATABASE_PORT, 10),
       synchronize: true,
       type: 'mysql',
-      username: databaseEP.split(':')[0] || process.env.DATABASE_USER
+      username: process.env.DATABASE_USER
     }),
     CompaniesModule,
     ProjectsModule,
