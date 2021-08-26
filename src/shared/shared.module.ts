a9cb55b6aeb46212ce27f3283ea9d2a582582e69
Juansecu <juansecoder@hotmail.com>
2021-08-26 15:55:33 -0500

    JwtService was added inside SharedModule

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { JwtService } from './services/jwt/jwt.service';
+
+@Module({
+  exports: [JwtService]
+})
 export class SharedModule {}
