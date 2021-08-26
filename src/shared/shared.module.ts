b2b742ee8ae990ba2f91f0b45a486e5e077f2f6a
Juansecu <juansecoder@hotmail.com>
2021-08-26 16:47:05 -0500

    JwtService was added as provider into SharedModule

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { JwtService } from './services/jwt/jwt.service';
 
 @Module({
+  providers: [JwtService],
   exports: [JwtService]
 })
 export class SharedModule {}
